36d6cb8e992a1787585a233e0a36ee0f0eb67e93
Prutha <Prutha@DESKTOP-GFUOTIS>
2022-03-21 13:05:58 +0530

    commit2

---
+++ TNSJAVA/src/corejava/Whileloop.java
@@ -0,0 +1,17 @@
+package corejava;
+
+public class Whileloop {
+
+	public static void main(String[] args) {
+		int i=99;
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+
+
+	}
+
+
---
+++ TNSJAVA/src/corejava/if_else.java
@@ -0,0 +1,23 @@
+package corejava;
+
+public class if_else {
+
+	public static void main(String[] args) {
+		
+		{
+			int x = 5;
+	          if(x != 5)
+	          {
+	        	  System.out.println("Value of x is not 5");
+	        	  
+	          }
+	          else
+	          {
+	          System.out.println("Value of x is 5");
+	          }
+		}
+
+
+	}
+
+}
